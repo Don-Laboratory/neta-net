29ad75f67938bf6dfd9fbd79fe59360b5a121c2d
fhiromasa <74556046+fhiromasa@users.noreply.github.com>
2024-02-07 14:43:53 +0900

    投稿用のページ、サインイン用のページそれぞれの雛形を作成しました。

---
+++ src/app/post/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "@/styles/page.module.css";
+const Post = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>投稿ページ</p>
+      </div>
+    </main>
+  );
+};
+export default Post;
