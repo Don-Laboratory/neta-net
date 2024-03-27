bcccbd34538234175e6b5b6a0efb58f6d39ad00a
yui.fujikawa <yui.fujikawa@micro-wave.net>
2024-03-27 15:26:47 +0900

    bootstrap設定　スライドバーも設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 import allScss from '@/styles/scss/project/_home.module.scss'
 
 export default function Home() {
@@ -11,59 +10,44 @@ export default function Home() {
         Push!test
       </button>
 
-      <div className={[allScss.sidebar].join(`d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary`)}>
-        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-          <svg className="bi pe-none me-2" width="40" height="32"></svg>
-          <span className="fs-4">Sidebar</span>
+      <div className={[allScss.sidebar].join(`flex-shrink-0 p-3`)}>
+        <a href="/" className="d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom">
+          <svg className="bi pe-none me-2" width="30" height="24"></svg>
+          <span className="fs-5 fw-semibold">Collapsible</span>
         </a>
-        <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item">
-            <a href="#" className="nav-link active" aria-current="page">
-              <svg className="bi pe-none me-2" width="16" height="16"></svg>
-              Home
-            </a>
+        <ul className="list-unstyled ps-0">
+          <li className="mb-1">
+            <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
+              ホーム
+            </button>
+            {/* <div className="collapse show" id="home-collapse">
+              <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                <li><a href="#" className="link-body-emphasis d-inline-flex text-decoration-none rounded">Overview</a></li>
+                <li><a href="#" className="link-body-emphasis d-inline-flex text-decoration-none rounded">Updates</a></li>
+                <li><a href="#" className="link-body-emphasis d-inline-flex text-decoration-none rounded">Reports</a></li>
+              </ul>
+            </div> */}
           </li>
-          <li>
-            <a href="#" className="nav-link link-body-emphasis">
-              <svg className="bi pe-none me-2" width="16" height="16"></svg>
-              Dashboard
-            </a>
+          <li className="mb-1">
+            <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
+              ランキング
+            </button>
           </li>
-          <li>
-            <a href="#" className="nav-link link-body-emphasis">
-              <svg className="bi pe-none me-2" width="16" height="16"></svg>
-              Orders
-            </a>
+          <li className="mb-1">
+            <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
+              カテゴリー
+            </button>
           </li>
-          <li>
-            <a href="#" className="nav-link link-body-emphasis">
-              <svg className="bi pe-none me-2" width="16" height="16"></svg>
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#" className="nav-link link-body-emphasis">
-              <svg className="bi pe-none me-2" width="16" height="16"></svg>
-              Customers
-            </a>
+          <li className="border-top my-3"></li>
+          <li className="mb-1">
+            <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
+              Account
+            </button>
           </li>
         </ul>
-        <div className="dropdown">
-          <a href="#" className="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <Image src="/pc_mv_img01.png" alt="" width="32" height="32" className="rounded-circle me-2" />
-            <strong>mdo</strong>
-          </a>
-          <ul className="dropdown-menu text-small shadow">
-            <li><a className="dropdown-item" href="#">New project...</a></li>
-            <li><a className="dropdown-item" href="#">Settings</a></li>
-            <li><a className="dropdown-item" href="#">Profile</a></li>
-            {/* <li><hr className="dropdown-divider"></li> */}
-            <li><a className="dropdown-item" href="#">Sign out</a></li>
-          </ul>
-        </div>
       </div>
 
-    </main >
+    </main>
 
   )
 }
