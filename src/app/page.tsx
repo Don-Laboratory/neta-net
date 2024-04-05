d7d299862e221623b87cc3a4c2c840ea74f7df3d
yui.fujikawa <yui.fujikawa@micro-wave.net>
2024-04-05 16:50:32 +0900

    ヘッダー作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/container';
 import NavBar from '@/components/layouts/tabs.tsx';
 import Sidebar from '@/components/layouts/sidebar.tsx';
+import HeaderHome from '@/components/layouts/headerHome.tsx';
 
 export default function Home() {
   return (
 
     <main>
+      <HeaderHome />
       <Image className={"mv"} src="/home/pc_mv_img01.png" alt="メインビジュアル" width={874} height={239} />
 
       <Container>
