6e07170ad73cb3edb9f878ed57d87bab1b3043cb
yui.fujikawa <yui.fujikawa@micro-wave.net>
2024-05-24 14:53:05 +0900

    react bootstrap error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 import Image from "next/image";
-import allScss from '@/styles/scss/project/_home.module.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/container';
 import NavBar from '@/components/layouts/tabs.tsx';
 import Sidebar from '@/components/layouts/sidebar.tsx';
 import HeaderHome from '@/components/layouts/headerHome.tsx';
 import Neta_Footer from '../components/layouts/Neta_Footer';
+import homeScss from '@/styles/scss/project/_home.module.scss';
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
       <Image className={"mv"} src="/home/pc_mv_img01.png" alt="メインビジュアル" width={874} height={239} />
 
       <Container>
-        <div className={`${allScss.threeCol}`}>
+        <div className={`${homeScss.threeCol}`}>
 
           <Sidebar />
 
